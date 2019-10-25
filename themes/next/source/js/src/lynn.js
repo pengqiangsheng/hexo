cf7076043f47c8a6b2caf70feb4d58940dca75cf
pqs <pengqiangsheng@leelen.cn>
2019-10-25 15:14:12 +0800

    新增 PWA 功能 ++

---
+++ themes/next/source/js/src/lynn.js
@@ -0,0 +1,5 @@
+
+
+console.log('\n' + ' %c Welcome to' + ' '  + ' %c http://inner.ink ' + '\n', 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; padding:5px 0;');
+
+console.log("%c ", "background: url(https://inner.ink/love/code-love-300x200.jpg) center;padding-left:290px;padding-bottom: 242px;")
